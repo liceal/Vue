ea516100cd06e408a0ec5917336edd62664e2370
liceal <675024132@qq.com>
2018-12-22 20:39:39 +0800

    12.22.20点39分 Vue练习更新

--- Vue练习/js/Vue_官网练习.js
+++ Vue练习/js/Vue_官网练习.js
@@ -42,9 +42,9 @@ var app4=new Vue({
 	 * */
 	el:'#app-4',data:{
 		todos:[
-			{text:'学习vue'},
-			{text:'学习laravel'},
-			{text:'成为小小全栈工程师'},
+			{text:'学习vue',number:0},
+			{text:'学习laravel',number:0},
+			{text:'成为小小全栈工程师',number:0},
 		]
 	}
 })
@@ -119,5 +119,159 @@ var app7=new Vue({
 			{id:1,text:'奶酪',money:50},
 			{id:2,text:'好吃的',money:99}
 		]
+	}
+})
+
+
+var app8=new Vue({
+	/**
+	 * 尝试把computed换成methods
+	 * methods方法：html中调用需要加()
+	 * 
+	 * */
+	el:'#app-8',data:{
+		message:'hello'
+	},
+	computed:{
+		reversedMessage:function(){
+			return this.message.split('').reverse().join('')
+		}
+	}
+})
+
+var app9=new Vue({
+	/**
+	 * 默认方法get与set会自动调用
+	 * 绑定id=app-9元素，设置数据属性，设置计算属性
+	 * 数据属性默认存在两个值
+	 * 计算属性中设置
+	 * 	键fullName,值写了get与set方法
+	 * 	在调用fullName后自动执行get，在对fullName赋值时自动调用set方法
+	 * 
+	 * get方法：返回Li ceal
+	 * set方法：对参数进行 ' ' 分割成数组
+	 * 	改写数据firsetName为参数分割后的第一组数据
+	 * 	改写数据lastName为参数分割后的最后一组数据
+	 *
+	 * */
+	el:'#app-9',data:{
+		firstName:'Li',
+		lastName:'ceal'
+	},computed:{
+		fullName:{
+			//getter
+			get:function(){
+				return this.firstName+' '+this.lastName
+			},
+			//setter
+			set:function(newValue){
+				var names=newValue.split(' ')
+				this.firstName=names[0]
+				this.lastName=names[names.length-1]
+			}
+		}
+	}
+})
+
+var app10=new Vue({
+	/**
+	 * 简单的watc监听器测试
+	 * watch内指定了question键，并提供方法
+	 * 	当question改变时，会执行这个方法
+	 * 	方法中传递了一个参数，为question的值
+	 * 	这里判断了question的内容来改变answer的值
+	 * */
+	el:'#app-10',data:{
+		question:'',
+		answer:'I cannot give you an answer until ask a question!'
+	},watch:{
+		question:function(value){
+			if(value==1){
+				this.answer='this content changed of 1'	
+			}else{
+				this.answer=value
+			}
+		}
+	}
+})
+
+
+var app11=new Vue({
+	/**
+	 * 简单的class与style绑定
+	 * 这里做了简单的效果，详情使用看html注解
+	 * class绑定  [className]:[false/true] 用boolean型来设置class是否绑定使用
+	 * 绑定id=app-11的元素 设置数据 设置计算 设置回调
+	 * 设置数据
+	 * 	control是输入框的内容，这里输入框的内容会返回到这个参数内存放，默认为空
+	 * 	isActive判断三个class是否绑定，默认都false不绑定
+	 * 	styleObject为style绑定，这里直接初始化值
+	 * 设置计算
+	 * 	classObject是为前端提供的绑定class的参数
+	 * 	这个参数内返回三种class是否被绑定
+	 * 	用function来返回值，return {键值内容}
+	 * 设置回调
+	 * 	对输入的内容进行获取判断
+	 * 	每次初始化三种判断三种class是否存放的数组为false
+	 * 	拆分输入的值，进行判断那个class需要使用
+	 * */
+	el:'#app-11',data:{
+		control:'',
+		isActive:[false,false,false],
+		styleObject:{
+			//TIPS:border-color在这里要borderColor。以大写代替-
+			color:'red',
+			borderColor: 'blue',
+			fontSize:'20px'
+		}
+	},computed :{
+		classObject:function(){
+			return {
+				active1:this.isActive[0],
+				active2:this.isActive[1],
+				active3:this.isActive[2],
+			}
+		}
+	},watch:{
+		control:function(value){
+			this.isActive=[false,false,false]
+			var options=value.split('')
+			for(var i=0;i<options.length;i++){
+				if(options[i]>=1&&options[i]<=3){
+					if(options[i]==1) this.isActive[1]=false
+					this.isActive[options[i]-1]=true
+				}
+			}
+		}
+	}
+})
+
+var app12=new Vue({
+	/**
+	 * 简单的for循环迭代案例参数
+	 * 详情看html注解
+	 * */
+	el:'#app-12',data:{
+		object:{
+			firstName: 'John',
+			lastName: 'Doe',
+			age: 30
+		}
+	}
+})
+
+/**
+ * 提供按钮事件的除法函数
+ * 可以自定义按钮时间 值为键码值
+ * 键码：https://www.cnblogs.com/hubgit/p/5794856.html
+ * */
+Vue.config.keyCodes.f1=112;
+var app13=new Vue({
+	el:'#app-13',data:{
+		
+	},methods:{
+		submit:function(){
+			alert('按键事件')
+		}
 	}
 })
